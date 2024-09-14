d7e69d1377e730dc9d17f27c512555def5b95598
shubham sharma <shubham232108@gmail.com>
2024-09-14 18:43:01 +0530

    env added

--- server/api/index.js
+++ server/api/index.js
@@ -3,16 +3,17 @@ import cors from 'cors'
 import Auth from '../routes/auth.js'
 import mongoose from 'mongoose'
 import Profile from '../routes/profile.js'
-
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-const port = 3045
+const port = process.env.PORT
 app.use(express.json())
 
 app.use(cors())
 
 
-mongoose.connect("mongodb+srv://shubham:qmPmj57GZjzMEMDn@cluster0.k6hoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.DATABASE_CON)
 
 app.use('/',Auth)
 app.use('/profile',Profile)
