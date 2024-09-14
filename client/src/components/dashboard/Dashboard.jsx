c003656b391995722075c1572820b60cd4d785d2
shubham sharma <shubham232108@gmail.com>
2024-09-15 03:14:08 +0530

    df

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -37,7 +37,7 @@ const Dashboard = () => {
       
       const requestData = { ...data, user: nameofuser.replaceAll('"','') };
       
-      let submit = await fetch(`http://localhost:3045/profile/profile/${nameofuser}`,{
+      let submit = await fetch(`https://backend-lyart-six.vercel.app/profile/profile/${nameofuser}`,{
         method: "POST",
         headers: {
           
@@ -58,7 +58,7 @@ const Dashboard = () => {
       async function fetch(){
         setloader(true)
         try {
-          let submit = await axios.get(`http://localhost:3045/profile/${nameofuser.replaceAll('"','')}`)
+          let submit = await axios.get(`https://backend-lyart-six.vercel.app/profile/${nameofuser.replaceAll('"','')}`)
         
          
          setdatainuse(submit.data)
