d4207ac8f54cbddcddfcd3a101410e2fc2fc5eb0
shubham sharma <shubham232108@gmail.com>
2024-09-14 18:05:22 +0530

    n

--- server/api/index.js
+++ server/api/index.js
@@ -3,11 +3,19 @@ import cors from 'cors'
 import Auth from '../routes/auth.js'
 import mongoose from 'mongoose'
 import Profile from '../routes/profile.js'
+import App from '../../client/src/App.jsx'
 
 const app = express()
 const port = 3045
 app.use(express.json())
-app.use(cors())
+const corsConfig = {
+  origin:"*",
+  Credential: true,
+  methods: ["GET","POST","PUT","DELETE"]
+}
+app.options("",cors(corsConfig))
+app.use(cors(corsConfig))
+
 
 mongoose.connect("mongodb+srv://shubham:qmPmj57GZjzMEMDn@cluster0.k6hoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
